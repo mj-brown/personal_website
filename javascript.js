6e05b72a3fa5be3f9524456703ad7beacedde181
mj-brown <brown8mj5@gmail.com>
2023-10-18 09:07:57 -1000

    finalized styling

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 //I want to create an element in my footer of each page.
-const h2 = document.createElement('h2');
+const h3 = document.createElement('h3');
 
 //I want that division to say a message.
-h2.innerHTML = 'This page is made with Aloha!';
-document.body.appendChild(h2);
+h3.innerHTML = 'This page is made with Aloha!';
+document.body.appendChild(h3);
